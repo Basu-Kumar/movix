f5bde6519e13f48ff3976663d8661eae3188bcbf
Basu Kumar <basutulsyan710@gmail.com>
2023-10-29 04:12:16 +0530

    header and footer complete

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import "./Home.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 };
